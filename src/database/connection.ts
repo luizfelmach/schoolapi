7c252ad091f53da3e7d7c5929936317f4e6306cc
Luiz Felipe Machado <56140722+luizfelmach@users.noreply.github.com>
2022-01-12 23:09:27 -0300

    Update connection in sequelize

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,8 +1,15 @@
 import { Sequelize } from "sequelize";
+import {
+  databaseHost,
+  databaseName,
+  databasePass,
+  databasePort,
+  databaseUser,
+} from "../configs/endpoints.config";
 
-const db = new Sequelize("api", "postgres", "postgres", {
-  host: "localhost",
-  port: 5432,
+const db = new Sequelize(databaseName, databaseUser, databasePass, {
+  host: databaseHost,
+  port: +databasePort,
   dialect: "postgres",
   logging: false,
 });

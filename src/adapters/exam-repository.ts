a246ddb2084a9ecf7ed8d43f2c72deb62a7a52af
Luiz Felipe Machado <56140722+luizfelmach@users.noreply.github.com>
2022-01-21 22:52:27 -0300

    Implement prisma on repository add exam

--- src/adapters/exam-repository.ts
+++ src/adapters/exam-repository.ts
@@ -1,9 +1,15 @@
 import { ExamModel } from "../domain/models";
 import { AddExam } from "../domain/rules";
+import { prismaClient } from "./prisma-client";
 
 export class ExamRepository implements AddExam {
   async add(data: Omit<ExamModel, "id">): Promise<ExamModel> {
-    const exam = { ...data, id: "ID UNIVERSAL" };
+    const { name } = data;
+    const exam = await prismaClient.exam.create({
+      data: {
+        name,
+      },
+    });
     return exam;
   }
 }
